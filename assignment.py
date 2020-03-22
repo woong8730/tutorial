6cc26e68a175ab3b4e31ba19fe256ccd53cd6503
woong8730 <wong8730@gmail.com>
2020-03-22 17:12:10 +0900

    assignment1

--- assignment.py
+++ assignment.py
@@ -12,8 +12,6 @@ EOF_list = [match.start() for match in re.finditer(re.escape(JPEG_EOF), data)]
 
 
 subdata = data[SOF_list[0]:EOF_list[0]+2]
-carve_filename = "Carve1_"+str(SOF_list[0])+"_"+str(EOF_list[0])+".jpg"
-carve_obj = open(carve_filename ,'wb')
+carve_obj = open('carved_image.jpg' ,'wb')
 carve_obj.write(subdata)
 carve_obj.close()
-print ("Found an image and carving it to "+carve_filename)
